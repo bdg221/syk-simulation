86d498645f7331b439c5b115195b22c7d9b8f403
Brian Goldsmith <brian.goldsmith@gmail.com>
2025-12-08 22:22:48 +0000

    Initial test for PPR

--- syk-simulation/ppr/test_ppr.py
+++ syk-simulation/ppr/test_ppr.py
@@ -5,7 +5,7 @@ import numpy as np
 from .ppr import PPR
 
 def test_ppr():
-    num_qubits = randint(1, 4)
+    num_qubits = randint(2, 4)
     x_mask = randint(0, 2**num_qubits - 1)
     z_mask = randint(0, 2**num_qubits - 1)
     theta = float(randint(0, 360))  # degrees

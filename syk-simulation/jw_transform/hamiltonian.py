2c2878a4485eeee645ee2a04a9a5c3e37af567f8
Larissa Kroell <larissa_kroell@outlook.com>
2026-01-27 12:44:27 +0000

    Updated JW transform for SYK model and added tests

--- syk-simulation/jw_transform/hamiltonian.py
+++ syk-simulation/jw_transform/hamiltonian.py
@@ -3,107 +3,71 @@ from math import floor
 from itertools import combinations
 import numpy as np
 
+def SYK_pair_to_mask(p: int, q: int):
+    """
+    Function generating the PauliString for the product of two majorana fermions
 
-def syk_majorana_to_mask(array: tuple | list):
-    '''
-    Function returning PauliMask corresponding to given product of Majorana fermions
-    
-    :param array: list in ascending order specifying the Majorana fermions
-    :type array: tuple | list
-    '''
+    :param p: Index for the first majorana fermion
+    :type p: int
 
-    array = list(array)
-    if array != sorted(array):
-        raise Exception("Indices must be in ascending order.")
-    if len(set(array))!=4:
-        raise Exception("Indices must be unique.")
-    
-    coefs = [floor(i/2) for i in array]
+    :param q: Index for the second majorana fermion
+    :type q: int
 
-    z_mask=0
-    x_mask=0
-    value=1
-
-    if coefs[1] == coefs[2]:
-        value*=-1
-
-        if range(coefs[0]+1, coefs[3]):
-            z_mask += sum([2**i for i in range(coefs[0]+1, coefs[1])])
-            z_mask += sum([2**i for i in range(coefs[1]+1, coefs[3])])
+    Output: List with coefficient in first entry and PauliMask in second
+    """
+    if q>p:    
+        if floor(q/2) == floor(p/2):
+            return [1j, PauliMask(0,2**floor(q/2))]
         else:
-            z_mask += 2**coefs[1]
-        
-        x_mask += 2**coefs[0] + 2**coefs[3]
-        if array[0]%2==0:
-            z_mask += 2**coefs[0]
-            value*=-1
-        if array[3]%2==1:
-            z_mask += 2**coefs[3]
-
-    else:
-        if range(coefs[0], coefs[1]):
-            value*=1j
-            z_mask += sum([2**i for i in range(coefs[0]+1, coefs[1])])
-            x_mask += 2**coefs[0]+2**coefs[1]
-            if array[0]%2==0:
-                z_mask +=2**coefs[0]
-                value*=-1
-            if array[1]%2==1:
-                z_mask +=2**coefs[1]
-        elif coefs[0]==coefs[1]:
-            z_mask += 2**coefs[0]
-            value *= 1j
+            x_mask = 2**floor(p/2)+2**floor(q/2)
+            z_mask = sum(2**i for i in range(floor(p/2)+1, floor(q/2)))
+            if q%2==1:
+                z_mask += 2**floor(q/2)
+            if p%2==0:
+                z_mask += 2**floor(p/2)
+                return [-1j, PauliMask(x_mask, z_mask)]
+            else:
+                return [1j, PauliMask(x_mask, z_mask)]
+    else: 
+        if floor(q/2) == floor(p/2):
+            return [-1j, PauliMask(0,2**floor(q/2))]
         else:
-            x_mask += 2**coefs[0]+2**coefs[1]
-            if array[0]%2==1:
-                z_mask += 2**coefs[0]
-            if array[1]%2==1:
-                z_mask += 2**coefs[1]
-    
-        if range(coefs[2], coefs[3]):
-            value*=1j
-            z_mask += sum([2**i for i in range(coefs[2]+1, coefs[3])])
-            x_mask += 2**coefs[2]+2**coefs[3]
-            if array[2]%2==0:
-                z_mask +=2**coefs[2]
-                value*=-1
-            if array[3]%2==1:
-                z_mask +=2**coefs[3]    
-        elif coefs[2]==coefs[3]:
-            z_mask += 2**coefs[2]
-            value *= 1j
-        else:
-            x_mask += 2**coefs[2]+2**coefs[3]
-            if array[2]%2==1:
-                z_mask += 2**coefs[2]
-            if array[3]%2==1:
-                z_mask += 2**coefs[3]
-   
-    return [value, PauliMask(x_mask, z_mask)]
-
+            x_mask = 2**floor(q/2)+2**floor(p/2)
+            z_mask = sum(2**i for i in range(floor(q/2)+1, floor(p/2)))
+            if p%2==1:
+                z_mask += 2**floor(p/2)
+            if q%2==0:
+                z_mask += 2**floor(q/2)
+                return [1j, PauliMask(x_mask, z_mask)]
+            else:
+                return [-1j, PauliMask(x_mask, z_mask)]
 
 def SYK_hamil(n: int, J: float=1, random_seed: int | None = None):
     """
     Function generating hamiltonian for the SYK model with 4-body interactions as a PauliSum
-    We are using the convention that the coupling constant is 4!*J where J is the coupling constant for all possible
-    combinations
+    We are using the convention that the coupling constant is 4!*J, where J is the coupling constant for all possible
+    combinations, i.e., we only consider indices in increasing order
 
-    :param n: Even integer specifying number of Majorana fermions
+    :param n: Integer specifying number of qubits (i.e., 2n Majorana fermions)
     :type n: int
 
     :param J: coupling constant
     :type J: float
+
+    :param random_seed(optional, default = none): Ability to set random_seed for testing/reproducability purposes
+    :type n: int
     """
 
     if random_seed is not None:
         np.random.seed(random_seed)
     
-    scale = np.sqrt(6/n**3)*J
+    scale = np.sqrt(6/(2*n)**3)*J
     hamil = PauliSum()
 
-    for tup in combinations(range(n),4):
-        pauli_op = syk_majorana_to_mask(tup)
+    for tup in combinations(range(2*n),4):
+        pauli_op1 = SYK_pair_to_mask(tup[0],tup[1])
+        pauli_op2 = SYK_pair_to_mask(tup[2],tup[3])
         coef = np.random.normal(loc=0, scale= scale, size = 1)
-        hamil.append([1/4*coef[0]*pauli_op[0], pauli_op[1]])
+        hamil.append([1/(96)*coef[0]*np.real(pauli_op1[0]*pauli_op2[0]), pauli_op1[1]*pauli_op2[1]])
 
     return hamil
--- syk-simulation/jw_transform/test_hamiltonian.py
+++ syk-simulation/jw_transform/test_hamiltonian.py
@@ -1,10 +1,37 @@
 from workbench_algorithms.utils import PauliMask, PauliSum 
 from math import floor 
 from itertools import combinations
-from .hamiltonian import syk_majorana_to_mask, SYK_hamil
+from .hamiltonian import SYK_pair_to_mask, SYK_hamil
 from scipy.special import comb
 
+import numpy as np
+from scipy.stats import kstest
 from pytest import mark
+
+
+def sign_coefficient(array: tuple | list):
+    #For testing the probability distribution in SYK model
+    parity = 1
+    coefs = [floor(i/2) for i in array]
+    par = [i%2 for i in array]
+    ln = len(set(coefs))
+
+    if (ln == 4) and (par[0] == par[2]):
+        parity*=-1
+    elif ln==3:
+        if (coefs[0]==coefs[1]) and (par[2]==1):
+            parity*=-1
+        elif par[0]==1:
+            parity*=-1
+    elif ln==2:
+        parity *=-1
+    return parity
+
+def mult_SYK_pairs(array):
+    pauli_op1 = SYK_pair_to_mask(array[0], array[1])
+    pauli_op2 = SYK_pair_to_mask(array[2], array[3])
+
+    return [np.real(pauli_op1[0]*pauli_op2[0]), pauli_op1[1]*pauli_op2[1]]
 
 @mark.parametrize(
     "coefficient, indices, pauli_string",
@@ -24,11 +51,81 @@ from pytest import mark
 )
 
 def test_jw_transform(coefficient, indices, pauli_string):
-    pauli_op = syk_majorana_to_mask(indices)
-    assert [pauli_op[0], pauli_op[1].get_pauli_string()] == [coefficient, pauli_string]
+    pauli_op1 = SYK_pair_to_mask(indices[0],indices[1])
+    pauli_op2 = SYK_pair_to_mask(indices[2],indices[3])
+
+    pauli_op = pauli_op1[1]*pauli_op2[1]
+    assert [pauli_op1[0]*pauli_op2[0], pauli_op.get_pauli_string()] == [coefficient, pauli_string]
+
+def test_SYK_pair_phase():
+    for i in range(50):
+        p = np.random.randint(0,200)
+        q = np.random.randint(0,200)
+        if p != q:
+            summand1 = SYK_pair_to_mask(p,q)
+            summand2=SYK_pair_to_mask(q,p)
+        assert summand1 == [-1*summand2[0], summand2[1]]
+
+def test_SYK_coefficient():
+    for i in range(100):
+        array = np.random.randint(0,200, size = 4)
+        while len(set(array))<4:
+            array = np.random.randint(0,200, size = 4)
+        array.sort()
+        summand = mult_SYK_pairs(array)
+        assert sign_coefficient(array) == summand[0]
+
+# def test_majorana_prod_phase():
+#     for i in range(100):
+#         array = np.random.randint(0,200, size = 4)
+#         while len(set(array))<4:
+#             array = np.random.randint(0,200, size = 4)
+#     #array.sort()
+#     #array_perm = np.random.permutation(array)
+#     #signum = permutation_sign(array_perm)
+
+#         array_perm =[ array[0], array[1], array[3], array[2]]
+
+#         flipped_summand = generate_SYK_summand(array_perm)
+#         if generate_SYK_summand(array)[0]!= -1*flipped_summand[0]:
+#             print(array)
+    
+#         assert generate_SYK_summand(array) == [-1*flipped_summand[0], flipped_summand[1]]
+
+def test_majorana_prod_string():
+    for i in range(100):
+        array = np.random.randint(0,200, size = 4)
+        while len(set(array))<4:
+            array = np.random.randint(0,200, size = 4)
+        array.sort()
+        array_perm = np.random.permutation(array)
+
+        assert mult_SYK_pairs(array_perm)[1] == mult_SYK_pairs(array)[1]
+
 
 def test_syk_summands():
     for i in range(2,8):
         theory = comb(2*i, 4)
-        hamil = SYK_hamil(2*i)
+        hamil = SYK_hamil(i)
         assert theory == len(hamil)
+
+def compare_syk_distribution(n: int, J: float, rtol=1e-4, atol=1e-6):
+    hamil = SYK_hamil(n, J= J, random_seed=42)
+    coefs = hamil.get_coefficients()
+
+    scale = np.sqrt(6/(2*n)**3)*J/96
+    res1 = kstest(coefs, np.random.normal(loc=0, scale = scale))
+
+    np.random.seed(42)
+    coefs_ideal = np.random.normal(loc=0, scale= scale, size = len(coefs))
+
+    assert res1[1]>0.05
+    assert np.allclose([0, np.var(coefs_ideal)], [np.mean(coefs), np.var(coefs)], rtol=rtol, atol=atol)
+
+def test_syk_statistics():
+     n=30
+     J=10
+     compare_syk_distribution(n=n, J=J)
+     
+
+    
